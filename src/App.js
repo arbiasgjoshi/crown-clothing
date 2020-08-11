88ed379da15da1ad0ec6152bb5ccd255b5494875
arbiasgj <arbiasgjoshi@gmail.com>
2020-08-11 17:31:43 +0200

    router and state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import './App.css';
 
 import Homepage from './modules/homepage/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+const HatsPage = () => (
+    <div>
+        <h1>Hats Page</h1>
+    </div>
+)
 
 function App() {
-  return (
-    <div className="wrapper">
-      <Homepage />
-    </div>
-  );
+    return (
+        <div className="wrapper">
+             <Switch>
+                <Route exact path='/' component={Homepage} />
+                <Route path='/hats' component={HatsPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
